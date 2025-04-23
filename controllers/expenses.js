d8bc6b018c612de8d3902fa2537945807f116ed5
Mazen Ahmed <106814868+MazenAhmed2@users.noreply.github.com>
2025-04-23 17:11:01 +0000

    Adding updateExpense controller

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,45 +1,60 @@
-import expenses from '../models/expenses.js'
-import users from '../models/users.js'
+import expenses from "../models/expenses.js";
+import users from "../models/users.js";
+import {ObjectId} from 'mongodb'
 
 export default {
-  
-  getAllExpenses : async function (req, res){
+  getAllExpenses: async function (req, res) {
     try {
-      res.status(200).json(await expenses.find({userId: req.userId}))
-    } catch(err){
-      console.log(err)
-      res.status(400).json({success : false})
+      res.status(200).json(await expenses.find({ userId: req.userId }));
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
     }
   },
 
-  getExpenseById: async function (req, res){
+  getExpenseById: async function (req, res) {
     try {
       // parse from request
-      const id = req.params.id
+      const id = req.params.id;
 
       // Get from db and send it to the client
-      res.status(200).json(await expenses.findById(id))
-      
-    } catch(err) {
-      console.log(err)
-      res.status(400).json({success : false})
+      res.status(200).json(await expenses.findById(id));
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
     }
-  }, 
+  },
 
-  createExpense: async function (req, res){
+  createExpense: async function (req, res) {
     try {
       // parse from data from body
-      const data = req.body
+      const data = req.body;
 
       // Add userId to data that will be inserted to db
-      data.userId = req.userId
+      data.userId = req.userId;
 
       // Create in db and Send response with the inserted object
-      res.status(200).json(await expenses.insertOne(data))
-
-    } catch(err) {
-      console.log(err)
-      res.status(400).json({success : false})
+      res.status(200).json(await expenses.insertOne(data));
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
     }
-  }
-}
+  },
+
+  updateExpense: async function (req, res) {
+    try {
+      // parse from request
+      const id = req.params.id;
+
+      // Parse date from req body
+      let data = req.body;
+
+      // Update in db
+      await expenses.updateOne({ _id: id }, data)
+      res.status(200).json((await expenses.find({ _id: id }))[0]);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ success: false });
+    }
+  },
+};
